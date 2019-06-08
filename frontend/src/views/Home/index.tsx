7f1d8ac7fd673b65c117a4f273a3723df47d20df
Minoyu <546354337@qq.com>
2019-06-08 21:39:46 +0800

    feature: 暂存 基本实现地图

--- frontend/src/views/Home/index.tsx
+++ frontend/src/views/Home/index.tsx
@@ -1,15 +1,20 @@
 import * as React from 'react';
 import * as style from "./style.scss";
 import bg from "./bg.svg"
+import NewsList from './NewsList';
+import { Switch, Route } from 'react-router';
+import NewsMap from './NewsMap';
 import Header from 'src/Components/Header';
 import AddButton from 'src/Components/AddButton';
-import NewsList from './NewsList';
 
 export default function Home() {
   return (
     <div className={style.homePage} style={{ backgroundImage: `url(${bg})` }}>
       <Header />
-      <NewsList />
+      <Switch>
+        <Route path="/list" component={NewsList} />
+        <Route component={NewsMap} />
+      </Switch>
       <AddButton />
     </div>
   )
