5a65de7c651706f775c50bce22ef68f18e8df779
fredzeng <979738295@qq.com>
2019-06-09 01:53:49 +0800

    feat: update db

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -20,7 +20,7 @@ import {
       password: 'root',
       database: 'fire',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+      // synchronize: true,
       logging: true,
     }),
     CacheModule.register(),
