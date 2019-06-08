4cb8bcde4a6309c4ddbd4fefcc9cf1e0d21cf099
Minoyu <546354337@qq.com>
2019-06-08 13:13:28 +0800

    init: init frontend

---
+++ frontend/src/views/LoadingPage/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Icon } from 'antd';
+import * as style from "./style.scss";
+
+export default function LoadingPage() {
+  return (
+    <div className={style.loadingPage}>
+      <Icon type="compass" style={{ fontSize: 45 }} theme="twoTone" spin={true} />
+      <div className={style.title}>
+        FireKeeper Loading
+      </div>
+      <div className={style.subtitle}>
+        首次加载较慢，请耐心等待
+      </div>
+    </div >
+  )
+}
