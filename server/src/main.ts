9bec9e138cd3d03284facdb83b3bdda023a86d32
fredzeng <979738295@qq.com>
2019-06-09 11:54:29 +0800

    feat: remove limit

--- server/src/main.ts
+++ server/src/main.ts
@@ -21,12 +21,12 @@ async function bootstrap() {
   app.use(helmet());
   // app.use(csurf());
   app.use(compression());
-  app.use(
-    rateLimit({
-      windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 100, // limit each IP to 100 requests per windowMs
-    }),
-  );
+  // app.use(
+  //   rateLimit({
+  //     windowMs: 15 * 60 * 1000, // 15 minutes
+  //     max: 100, // limit each IP to 100 requests per windowMs
+  //   }),
+  // );
   await app.listen(3000);
 }
 bootstrap();
