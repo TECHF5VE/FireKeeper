8415e9a86f92f0b09330a7f702f94b1898dc6244
fredzeng <979738295@qq.com>
2019-06-09 09:56:27 +0800

    feat: update  schedule

--- server/src/schedule/schedule.service.ts
+++ server/src/schedule/schedule.service.ts
@@ -1,12 +1,23 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Interval, NestSchedule } from 'nest-schedule';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { News } from '../news/news.entity';
 
 @Injectable()
 export class ScheduleService extends NestSchedule {
-  @Interval(2000)
-  intervalJob() {
+  constructor(
+    @InjectRepository(News) private readonly newsRepository: Repository<News>,
+  ) {
+    super();
+  }
+
+  @Interval(60000)
+  async intervalJob() {
     Logger.log('executing interval job');
     // if you want to cancel the job, you should return true;
-    return true;
+    const list = await this.newsRepository.find();
+    list.forEach(item => (item.weight -= 0.02));
+    this.newsRepository.save(list);
   }
 }
